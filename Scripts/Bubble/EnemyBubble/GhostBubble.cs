f94af4ec4cb659b979a01a33eb78e49f17319598
MDA21 <1399443067@qq.com>
2026-01-31 20:31:51 +0800

    fix: fade out and destroy

--- Scripts/Bubble/EnemyBubble/GhostBubble.cs
+++ Scripts/Bubble/EnemyBubble/GhostBubble.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class GhostBubble : MonoBehaviour
 {
-    public float hideDelay = 20f;
+    public float hideDelay = 3f;
+    public float fadeDuration = 1f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // 让碰撞到的物体忽略与此物体的碰撞
-            StartCoroutine(HideAfterDelay(hideDelay));
+            FadeOutAndDestroy fade = GetComponent<FadeOutAndDestroy>();
+            if (fade == null)
+            {
+                fade = gameObject.AddComponent<FadeOutAndDestroy>();
+            }
+            fade.startFadeDelay = hideDelay;
+            fade.fadeDuration = fadeDuration;
 
         }
     }
-
-
-    private IEnumerator HideAfterDelay(float time)
-        {
-            yield return new WaitForSeconds(time*Time.fixedDeltaTime);
-            gameObject.SetActive(false);
-        }
 
 }
