4ec8b339a8d827fa6ffb3474a717431e26539013
MDA21 <1399443067@qq.com>
2026-02-01 17:25:03 +0800

    game over

---
+++ Scripts/UI/GameOverController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class GameOverController : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    [Tooltip("The name of the level scene to restart (e.g. Level1)")]
+    public string levelSceneName = "Level1";
+    
+    [Tooltip("The name of the main menu scene")]
+    public string mainMenuSceneName = "MainMenu";
+
+    public void RestartGame()
+    {
+        // Restart the level
+        SceneManager.LoadScene(levelSceneName);
+        
+        // Optional: click sound
+        if (AudioManager.Instance != null)
+        {
+            // AudioManager.Instance.Play("UIClick"); 
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+        
+        // Optional: click sound
+        if (AudioManager.Instance != null)
+        {
+            // AudioManager.Instance.Play("UIClick"); 
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
